d27dfb5f99f61abfe9c8a5203bb579aaec99cef5
Jean BaMaung <jbamaung17@gmail.com>
2018-03-13 17:38:43 +0000

    created rest of CRUD functions

--- src/main/java/DB/DBHelper.java
+++ src/main/java/DB/DBHelper.java
@@ -1,9 +1,8 @@
 package DB;
 
-import org.hibernate.Hibernate;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.hibernate.*;
+
+import java.util.List;
 
 
 public class DBHelper {
@@ -23,6 +22,76 @@ public class DBHelper {
         } finally {
             session.close();
         }
+    }
+
+    private static void update(Object object) {
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction.begin();
+            session.update(object);
+            transaction.commit();
+        } catch (HibernateException ex) {
+            transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    private static void delete(Object object) {
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction.begin();
+            session.delete(object);
+            transaction.commit();
+        } catch (HibernateException ex) {
+            transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public static <T> List<T> getList(Criteria cr) {
+        List<T> results = null;
+        try {
+            transaction = session.beginTransaction();
+            results = cr.list();
+            transaction.commit();
+        } catch (HibernateException ex) {
+            transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return results;
+    }
+
+    public static <T> T getUniqueResult(Criteria cr) {
+        T result = null;
+        try {
+            transaction = session.beginTransaction();
+            result = (T) cr.uniqueResult();
+            transaction.commit();
+
+        } catch (HibernateException ex) {
+            transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return result;
+    }
+
+
+
+    private static <T> List<T> getAll(Class classType) {
+        session = HibernateUtil.getSessionFactory().openSession();
+        List<T> results = null;
+        transaction.begin();
+        Criteria cr = session.createCriteria(classType);
+        results = getList(cr);
+        return results;
     }
 
 }
